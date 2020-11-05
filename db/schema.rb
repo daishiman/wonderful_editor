ce72331f0b8c22713d41151b58e067a13b85e04f
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-05 17:05:26 +0900

    rails erd 実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20_201_105_020_022) do
+ActiveRecord::Schema.define(version: 2020_11_05_020022) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "article_likes", :force => :cascade do |t|
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
-    t.bigint "article_id", :null => false
-    t.bigint "user_id", :null => false
-    t.index ["article_id"], :name => "index_article_likes_on_article_id"
-    t.index ["user_id"], :name => "index_article_likes_on_user_id"
+  create_table "article_likes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["article_id"], name: "index_article_likes_on_article_id"
+    t.index ["user_id"], name: "index_article_likes_on_user_id"
   end
 
-  create_table "articles", :force => :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
-    t.bigint "user_id", :null => false
-    t.index ["user_id"], :name => "index_articles_on_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "comments", :force => :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
-    t.bigint "user_id", :null => false
-    t.bigint "article_id", :null => false
-    t.index ["article_id"], :name => "index_comments_on_article_id"
-    t.index ["user_id"], :name => "index_comments_on_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "users", :force => :cascade do |t|
-    t.string "provider", :default => "email", :null => false
-    t.string "uid", :default => "", :null => false
-    t.string "encrypted_password", :default => "", :null => false
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", :default => false
+    t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
@@ -58,12 +59,12 @@ ActiveRecord::Schema.define(:version => 20_201_105_020_022) do
     t.string "image"
     t.string "email"
     t.json "tokens"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
-    t.index ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-    t.index ["email"], :name => "index_users_on_email", :unique => true
-    t.index ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-    t.index ["uid", "provider"], :name => "index_users_on_uid_and_provider", :unique => true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "article_likes", "articles"

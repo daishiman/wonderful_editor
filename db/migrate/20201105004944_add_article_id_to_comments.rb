bc564a8372912b2c3bc90825afd1418ee74a791d
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-05 10:46:10 +0900

    comment table の article_id の設定変更

--- db/migrate/20201105004944_add_article_id_to_comments.rb
+++ db/migrate/20201105004944_add_article_id_to_comments.rb
@@ -1,6 +1,6 @@
 class AddArticleIdToComments < ActiveRecord::Migration[6.0]
   def change
     add_reference :comments, :article, :foreign_key => true
-    change_column_null :comments, :user_id, false
+    change_column_null :comments, :article_id, false
   end
 end

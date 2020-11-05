73044a46725ccd0ab86b6a46ccb7cd50b9b780d2
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-05 11:08:42 +0900

    rubocop 実行

--- db/migrate/20201105015421_add_article_id_to_article_likes.rb
+++ db/migrate/20201105015421_add_article_id_to_article_likes.rb
@@ -1,6 +1,6 @@
 class AddArticleIdToArticleLikes < ActiveRecord::Migration[6.0]
   def change
-    add_reference :article_likes, :article, foreign_key: true
+    add_reference :article_likes, :article, :foreign_key => true
     change_column_null :article_likes, :article_id, false
   end
 end

ced7031c46b114fedda4bc230e4b13acba64cfe7
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-06 17:13:38 +0900

    テスト用のヘルパーメソッドを使う際に記述するファイル置き場 を追記

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,8 @@ Faker::Config.locale = :ja # 追加
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+#テスト用のヘルパーメソッドを使う際に記述するファイル置き場
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.

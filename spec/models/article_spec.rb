542b36426ea6026bf43fe60acce78a3cd94cb195
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-09 10:07:26 +0900

    article テスト記載（途中）

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -21,4 +21,32 @@ require "rails_helper"
 
 RSpec.describe Article, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "すべて記載しているとき" do
+    let(:article) { create(:article) }
+    it "article が作成できる" do
+      expect(article).to be_valid
+    end
+  end
+
+  # context "body が記載されていないとき" do
+  #   binding.pry
+  #   let(:article) { build(:article, :body => nil) }
+  #   it "article が作成できない" do
+  #     expect(article).to be_invalid
+  #     binding.pry
+  #   end
+  # end
+
+  # context "title が記載されていないとき" do
+  #   it "article が作成できない" do
+
+  #   end
+  # end
+
+  # context "user_id が記載されていないとき" do
+  #   it "article が作成できない" do
+
+  #   end
+  # end
 end

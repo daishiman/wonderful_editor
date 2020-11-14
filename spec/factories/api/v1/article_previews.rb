c37f369aee3af14cdc75d059cb2d2e3ab82e38c4
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-14 11:14:02 +0900

    rubocop 実行

--- spec/factories/api/v1/article_previews.rb
+++ spec/factories/api/v1/article_previews.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :api_v1_article_preview, class: 'Api::V1::ArticlePreview' do
+  factory :api_v1_article_preview, :class => "Api::V1::ArticlePreview" do
     title { "MyString" }
     updated_at { "" }
   end
--- spec/models/api/v1/article_preview_spec.rb
+++ spec/models/api/v1/article_preview_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Api::V1::ArticlePreview, type: :model do
+RSpec.describe Api::V1::ArticlePreview, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end

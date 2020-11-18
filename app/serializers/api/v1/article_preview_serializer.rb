4d835479f6a28e7cc6f64615a5747f705fecf386
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-18 11:38:23 +0900

    rubocop 実行

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -3,5 +3,4 @@ class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
 
   has_many :users, :serializer => Api::V1::UserSerializer
   has_many :articles, :serializer => Api::V1::ArticleSerializer
-
 end
--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -3,5 +3,4 @@ class Api::V1::ArticleSerializer < ActiveModel::Serializer
 
   belongs_to :user, :serializer => Api::V1::UserSerializer
   belongs_to :article_previewa, :serializer => Api::V1::ArticlePreviewSerializer
-
 end

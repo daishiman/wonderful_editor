e84615d576a6bbe3b611e6b05f8c2a512a5331bb
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-18 11:29:41 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
       resources :article_previews
     end
   end
+  namespace :api do
+    namespace :v1 do
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do

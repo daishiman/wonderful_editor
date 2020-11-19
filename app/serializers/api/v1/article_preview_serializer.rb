9b95d61a2f1ab5d41ec0b6ec7f58badd20181887
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-11-19 15:38:16 +0900

    article_preview は article の一覧なので作る必要ない

--- app/serializers/api/v1/article_preview_serializer.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :updated_at
-
-  has_many :users, :serializer => Api::V1::UserSerializer
-  has_many :articles, :serializer => Api::V1::ArticleSerializer
-end

65ad3ac3bff96c56a48c3ebcbd9ff8cf024fe616
daishiman <daishimanju@gmail.com>
2020-11-20 16:40:19 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :article_previews
-    end
-  end
-  namespace :api do
-    namespace :v1 do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

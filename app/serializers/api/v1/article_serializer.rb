87e54f8d42e3c40c1e55acade74315786faca21c
daishiman <daishimanju@gmail.com>
2020-11-24 16:36:43 +0900

    binding.pry 削除

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,8 +1,5 @@
 class Api::V1::ArticleSerializer < ActiveModel::Serializer
-  binding.pry
   attributes :id, :title, :body, :updated_at
 
-  binding.pry
   belongs_to :user, :serializer => Api::V1::UserSerializer
-  binding.pry
 end

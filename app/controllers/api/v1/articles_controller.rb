7d00cf0afa48715e0794b32ed69097c6bf6c5768
daishiman <daishimanju@gmail.com>
2020-11-26 16:18:28 +0900

    create メソッドの実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,13 +1,27 @@
 module Api::V1
   class ArticlesController < BaseApiController
+    # before_action :authenticate_user!, only: [:create, :update, :destroy]
+
     def index
-      @articles = Article.order(:updated_at => :desc)
-      render :json => @articles, :each_serializer => Api::V1::ArticlePreviewSerializer
+      articles = Article.order(:updated_at => :desc)
+      render :json => articles, :each_serializer => Api::V1::ArticlePreviewSerializer
     end
 
     def show
-      @article = Article.find(params[:id])
-      render :json => @article
+      article = Article.find(params[:id])
+      render :json => article
     end
+
+    def create
+      @article = current_user.articles.create!(article_params)
+      render json: @article, serializer: Api::V1::ArticleSerializer
+    end
+
+    private
+
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
+
   end
 end

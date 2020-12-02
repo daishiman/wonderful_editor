b4636d90048aadca2e87194ecb99047387229cdc
daishiman <daishimanju@gmail.com>
2020-12-02 11:28:33 +0900

    rubocop 実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root :to => "home#index"
 
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
+      mount_devise_token_auth_for "User", :at => "auth"
       resources :articles
     end
   end

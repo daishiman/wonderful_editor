59e44bd37ddf8b34251a04cfc2bdf1b6d733ab54
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-06 16:06:32 +0900

    新規登録 API の実装

---
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Auth::Registrations", type: :request do
+  describe "POST /api_v1_auth_registration" do
+    subject { post(api_v1_user_registration_path, params: params) }
+    fcontext "登録情報が揃っているとき" do
+      let(:params) { attributes_for(:user) }
+
+      it "新規登録できる" do
+        expect { subject }.to change { User.count }.by(1)
+
+        res_body = JSON.parse(response.body)
+        expect(res_body["data"]["name"]).to eq params[:name]
+        expect(res_body["data"]["email"]).to eq params[:email]
+
+        res_header = response.header
+        expect(res_header).to include "access-token"
+        expect(res_header).to include "client"
+        expect(res_header).to include "expiry"
+        expect(res_header).to include "uid"
+        expect(res_header).to include "token-type"
+
+        expect(response).to have_http_status(200)
+      end
+
+    end
+
+    fcontext "name が存在しないとき" do
+      let(:params) { attributes_for(:user, name: nil) }
+
+      it "エラーする" do
+        expect { subject }.to change { User.count }.by(0)
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(res["errors"]["name"]).to include "can't be blank"
+      end
+    end
+
+    fcontext "email が存在しないとき" do
+      let(:params) { attributes_for(:user, email: nil) }
+
+      it "エラーする" do
+        expect { subject }.to change { User.count }.by(0)
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(res["errors"]["email"]).to include "can't be blank"
+      end
+    end
+
+    fcontext "password が存在しないとき" do
+      let(:params) { attributes_for(:user, password: nil) }
+
+      it "エラーする" do
+        expect { subject }.to change { User.count }.by(0)
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(res["errors"]["password"]).to include "can't be blank"
+      end
+    end
+
+
+  end
+end

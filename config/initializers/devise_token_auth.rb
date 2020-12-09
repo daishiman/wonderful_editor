4feed2b2d4f1da91e8e81709931fe910e8939965
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-10 06:51:27 +0900

    stash 実行

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -44,17 +44,23 @@ DeviseTokenAuth.setup do |config|
   # Makes it possible to change the headers names
   # 認証用ヘッダーの名前の定義です
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   config.headers_names = { :'access-token' => "access-token",
                            :client => "client",
                            :expiry => "expiry",
                            :uid => "uid",
                            :'token-type' => "token-type" }
 =======
+=======
+>>>>>>> Stashed changes
   config.headers_names = {:'access-token' => 'access-token',
                          :'client' => 'client',
                          :'expiry' => 'expiry',
                          :'uid' => 'uid',
                          :'token-type' => 'token-type' }
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
   # By default, only Bearer Token authentication is implemented out of the box.

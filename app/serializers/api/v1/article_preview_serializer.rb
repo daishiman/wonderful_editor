1fd1a073de57a1aaa935c2d2da03c5ca18e1ff72
万壽本大嗣 <manju.manju.03.28@gmail.com>
2020-12-26 16:28:49 +0900

    下書き記事の一覧/詳細 のAPI実装

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :updated_at, :status
+  attributes :id, :title, :body, :updated_at, :status
 
   belongs_to :user, :serializer => Api::V1::UserSerializer
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(
+  :name => "first",
+  :email => "first@example.com",
+  :password => "password",
+)
+
+user2 = User.create!(
+  :name => "second",
+  :email => "second@example.com",
+  :password => "password",
+)
+
+user1.articles.create!(
+  :title => "title1",
+  :body => "body1",
+  :status => "draft",
+)
+
+user2.articles.create!(
+  :title => "title2",
+  :body => "body2",
+  :status => "draft",
+)
+
+user1.articles.create!(
+  :title => "title3",
+  :body => "body3",
+  :status => "published",
+)
+
+user2.articles.create!(
+  :title => "title4",
+  :body => "body4",
+  :status => "published",
+)
